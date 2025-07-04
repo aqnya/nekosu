f07d7fd149205e217e8daeeaba720fb1e1b3faab
BUG <bugcc5285@gmail.com>
2025-07-04 16:43:45 +0800

    Add new options

--- app/src/main/java/me/neko/nzhelper/ui/screens/SettingsScreen.kt
+++ app/src/main/java/me/neko/nzhelper/ui/screens/SettingsScreen.kt
@@ -6,18 +6,15 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -25,16 +22,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalUriHandler
-import androidx.compose.ui.text.LinkAnnotation
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.DialogProperties
+import me.neko.nzhelper.ui.util.AboutDialog
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -91,89 +81,6 @@ fun SettingsScreen() {
             AboutDialog(onDismiss = { showAboutDialog = false })
         }
     }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun AboutDialog(onDismiss: () -> Unit) {
-    // 动态读取版本号
-    val context = LocalContext.current
-    val versionName = remember {
-        context.packageManager
-            .getPackageInfo(context.packageName, 0)
-            .versionName
-    }
-
-    val uriHandler = LocalUriHandler.current
-    val url = "https://github.com/zzzdajb/DickHelper"
-    val label = "DickHelper"
-
-    val sourceLabel = "GitHub"
-    val sourceUrl = "https://github.com/bug-bit/NzHelper"
-
-    val annotated = buildAnnotatedString {
-        append("这是一个以开源项目 ")
-
-        val start = length
-        append(label)
-        val end = length
-
-        addStyle(
-            style = SpanStyle(textDecoration = TextDecoration.Underline),
-            start = start,
-            end = end
-        )
-        addLink(
-            LinkAnnotation.Clickable(
-                tag = label,
-                linkInteractionListener = { uriHandler.openUri(url) }
-            ),
-            start = start,
-            end = end
-        )
-
-        append(" 作为参考的使用 Kotlin 编写的 App")
-    }
-    val viewSource = buildAnnotatedString {
-        append("在 ")
-        val start = length
-        append(sourceLabel)
-        val end = length
-        addStyle(
-            style = SpanStyle(textDecoration = TextDecoration.Underline),
-            start = start,
-            end = end
-        )
-        addLink(
-            LinkAnnotation.Clickable(
-                tag = sourceLabel,
-                linkInteractionListener = { uriHandler.openUri(sourceUrl) }
-            ),
-            start = start,
-            end = end
-        )
-        append(" 查看源码")
-    }
-
-    AlertDialog(
-        onDismissRequest = onDismiss,
-        title = { Text("关于本应用") },
-        text = {
-            Column {
-                Text("版本：$versionName")
-                Spacer(Modifier.height(8.dp))
-                Text(text = annotated, style = TextStyle())
-                Spacer(Modifier.height(8.dp))
-                Text(text = viewSource, style = TextStyle())
-            }
-        },
-        confirmButton = {
-            TextButton(onClick = onDismiss) {
-                Text("确定")
-            }
-        },
-        properties = DialogProperties(usePlatformDefaultWidth = true)
-    )
 }
 
 @Preview(showBackground = true)
