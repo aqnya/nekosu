c6d5c899bfd183c94152d3463b5dd55a9acf88a4
Aqnya <siyueba73@gmail.com>
2025-09-25 22:46:37 +0800

    add logcat.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,6 +90,7 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
+     implementation("androidx.compose.material:material-icons-extended:1.7.3")
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.gson)
