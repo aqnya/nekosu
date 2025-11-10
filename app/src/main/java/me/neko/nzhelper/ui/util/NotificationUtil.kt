5abb3eeb0838582a6ec653394fdf96a4e93bb5ce
Aqnya <siyueba73@gmail.com>
2025-11-10 15:13:25 +0800

    update cpp.

---
+++ app/src/main/java/me/neko/nzhelper/ui/util/NotificationUtil.kt
@@ -0,0 +1,23 @@
+package me.neko.nzhelper.ui.util
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+
+object NotificationUtil {
+    const val CHANNEL_ID = "NzHelper"
+    fun createChannel(context: Context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "NzHelper"
+            val descriptionText = "NzHelper"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+}
